5d3afd221e20f5e12e7881011108bb77d6736b07
Ilya Kompaniets <jenkslex@gmail.com>
2018-09-15 16:43:47 +0300

    Added migration script

--- perftracker/migrations/0029_auto_20180914_1542.py
+++ perftracker/migrations/0029_auto_20180914_1542.py
@@ -4,6 +4,17 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
+def linkJobToRegression(apps, schema_editor):
+    JobModel        = apps.get_model('perftracker', 'JobModel')
+    RegressionModel = apps.get_model('perftracker', 'RegressionModel')
+
+    for regression in RegressionModel.objects.all():
+        for job in JobModel.objects.filter(regression_tag=regression.tag).all():
+            job.regression_original = regression
+            job.regression_linked   = regression
+            job.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,10 +22,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='jobmodel',
-            name='regression_tag',
-        ),
         migrations.AddField(
             model_name='jobmodel',
             name='regression_linked',
@@ -24,5 +31,12 @@ class Migration(migrations.Migration):
             model_name='jobmodel',
             name='regression_original',
             field=models.ForeignKey(blank=True, help_text='Original regression of this job', null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='regr_original', to='perftracker.RegressionModel'),
+        ),
+        migrations.RunPython(
+            linkJobToRegression
+        ),
+        migrations.RemoveField(
+            model_name='jobmodel',
+            name='regression_tag',
         ),
     ]
