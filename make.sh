cb674448009a8f842001fba0b67d2463d8fdbf3c
perfguru87 <perfguru87@gmail.com>
2018-10-02 02:30:34 +0300

    MINOR - fix a misprint in make.sh

--- make.sh
+++ make.sh
@@ -4,5 +4,5 @@ VER=`grep "__pt_version__ = " perftracker/__init__.py | cut -d "'" -f 2`
 echo "\n###### run the following commands manually ######\n"
 echo "vim perftracker/__init__.py  # update version"
 echo "git commit -m \"bump version to $VER\" perftracker/__init__.py && git tag \"v$VER\" && git push origin --tags"
-echp "git push"
+echo "git push"
 echo
