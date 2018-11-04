eba349c063ec0b560a758553289aecbabbf795ca
perfguru87 <perfguru87@gmail.com>
2018-11-04 13:21:21 +0300

    make.sh - added README.md to list of committed files

--- make.sh
+++ make.sh
@@ -4,6 +4,6 @@ VER=`grep "__pt_version__ = " perftracker/__init__.py | cut -d "'" -f 2`
 echo "\n###### run the following commands manually ######\n"
 echo "vim perftracker/__init__.py  # update version"
 echo "vim README.md  # update version"
-echo "git commit -m \"bump version to $VER\" perftracker/__init__.py && git tag \"v$VER\" && git push origin --tags"
+echo "git commit -m \"bump version to $VER\" perftracker/__init__.py README.md && git tag \"v$VER\" && git push origin --tags"
 echo "git push"
 echo
